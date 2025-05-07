2ac72504b675547f5fbebe424178565dad3f5769
dgjun32 <superbench@costsim-dev-000002.mvqiehgheaduxijk1qwjzeny4g.ax.internal.cloudapp.net>
2025-05-07 04:59:45 +0000

    dd

--- pyan/analyzer.py
+++ pyan/analyzer.py
@@ -219,10 +219,11 @@ class CallGraphVisitor(ast.NodeVisitor):
                             and from_node.flavor == Flavor.IMPORTEDITEM
                         ):
                             # use define edges as potential candidates
-                            for candidate_to_node in self.defines_edges[to_node]:  #
-                                if candidate_to_node.name == node.name:
-                                    attribute_import_mapping[node] = candidate_to_node
-                                    break
+                            if to_node in self.defines_edges:
+                                for candidate_to_node in self.defines_edges[to_node]:  #
+                                    if candidate_to_node.name == node.name:
+                                        attribute_import_mapping[node] = candidate_to_node
+                                        break
         import_mapping.update(attribute_import_mapping)
 
         # remap nodes based on import mapping
